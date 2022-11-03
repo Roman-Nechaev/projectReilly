9826f0cbd52d5c6b7962edbbacd944fb67b407d9
Roman-Nechaev <nrchaevroman@gmail.com>
2022-11-04 00:17:11 +0200

    gql

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 // index.js
 // This is the main entry point of our application
+
 const express = require('express');
-const app = express();
+const { ApolloServer, gql } = require('apollo-server-express');
+
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello World Server!!!'));
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-app.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}`)
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const app = express();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({
+  app,
+  path: '/api',
+});
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
 );
+
+// app.get('/', (req, res) => res.send('Hello World Server!!!'));
+
+// app.listen(port, () =>
+//   console.log(`Server running at http://localhost:${port}`)
+// );
