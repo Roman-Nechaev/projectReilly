64af2870b14917c3063926a77c7decac1cc1a5c4
Roman-Nechaev <nrchaevroman@gmail.com>
2022-11-07 00:18:18 +0200

    сборка GQL / apollo-server-express

--- src/index.js
+++ src/index.js
@@ -6,15 +6,50 @@ const { ApolloServer, gql } = require('apollo-server-express');
 
 const port = process.env.PORT || 4000;
 
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  {
+    id: '2',
+    content: 'This is a another note',
+    author: 'Harlow Everly',
+  },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' },
+];
+
 const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
+    notes: () => notes,
+    note: (perent, args) => {
+      return notes.find(note => note.id === args.id);
+    },
+  },
+  Mutation: {
+    newNote: (perent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott',
+      };
+      notes.push(noteValue);
+      return noteValue;
+    },
   },
 };
 
